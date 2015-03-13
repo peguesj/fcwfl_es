aa8da44699c8683f8a4f75d926113952e19f1032
peguesj <jeremiah.pegues@gmail.com>
2015-03-13 16:52:27 -0400

    upd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # heroku needs this?
+  config.assets.compile = true
 end
