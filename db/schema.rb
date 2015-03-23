eef204341b6750819cf6e47b4f8c5c56b0ca4220
behold <jeremiah.pegues@gmail.com>
2015-03-23 16:18:17 -0400

    User OmniAuth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313212414) do
+ActiveRecord::Schema.define(version: 20150323201722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150313212414) do
     t.integer  "role"
     t.integer  "currentLat"
     t.integer  "currentLong"
+    t.string   "picture"
   end
 
   create_table "yelpfinds", force: true do |t|
